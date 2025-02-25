36ff4f2ba4d0761b248a28f09942ad557630399d
Kira Wulff <kira@kirawulff.space>
2025-02-25 11:58:50 -0700

    Last commit I'm making. Should be done

---
+++ Croppenemgoodepisode3.ijm
@@ -0,0 +1,2 @@
+makeRectangle(1956, 1590, 120, 120);
+run("Crop");
--- lab7.m
+++ lab7.m
@@ -1,4 +1,3 @@
-
 %% Problem 1
 %Problem 1
 % a.
@@ -8,6 +7,8 @@ test3 = ifftshift(test2)
 
 % b
 figure
+imagesc(SquareWave);
+figure;
 X = fft2(fftshift(SquareWave));
 
 % c
@@ -16,7 +17,12 @@ aticklen = 200e-3*633e-9/(4e-6);
 % atickstart = (length(X)-1)*aticklen/2;
 % atick = linspace(-atickstart,atickstart, 5);
 Z = imresize(Y, aticklen);
+imagesc(Y)
+saveas(gcf,'out/Step1Fig1.jpg');
+figure;
 imagesc(Z)
+saveas(gcf,'out/Step1Fig2.jpg');
+figure;
 
 % d
 r_lens = .254/2;
@@ -36,8 +42,13 @@ for index1 = 0:length(mask)
   end
 end
 imagesc(mask);
+saveas(gcf,'out/Step1Fig3.jpg');
 final = mask.*Z;
 imagesc(final);
+saveas(gcf,'out/Step1Fig4.jpg');
+realfinal = real(ifftshift(ifft2(fftshift(final))));
+imagesc(realfinal)
+saveas(gcf, 'out/Step1Fig5.jpg');
 
 %% Part 2
 
@@ -86,7 +97,7 @@ d_px = d/aticklen
 imagesc(mask);
 final = mask.*Z;
 imagesc(final);
-        
+saveas(gcf, 'out/Step2Fig1.jpg');
 
 %% Part 3
 Hologram = imread('CT_Hologram.jpg');
@@ -99,6 +110,7 @@ figure
 X = fft2(mat2gray(fftshift(Z)));
 Y_Hologram = abs(log10(ifftshift(X).^2));
 imagesc(Y_Hologram);
+saveas(gcf, 'out/Step3Fig1.jpg');
 
 % With subtraction
 Z = rgb2gray(Hologram)-(rgb2gray(Reference)+rgb2gray(Object));
@@ -106,6 +118,7 @@ figure
 X = fft2(mat2gray(fftshift(Z)));
 Y_Hologram = abs(log10(ifftshift(X).^2));
 imagesc(Y_Hologram);
+saveas(gcf, 'out/Step3Fig2.jpg');
 
 %% Problem 4
 
@@ -118,7 +131,9 @@ Z = rgb2gray(Hologram)-(rgb2gray(Reference)+rgb2gray(Object));
 figure
 X = fft2(mat2gray(fftshift(Z)));
 Y_Hologram = abs(log10(ifftshift(X).^2));
+
 imagesc(Y_Hologram);
+saveas(gcf, 'out/Step4Fig1.jpg');
 
 x = linspace(-1,1,length(Hologram));
 y = linspace(-1,1,length(Hologram));
@@ -149,3 +164,55 @@ X = fft2(mat2gray(fftshift(Z)));
 Y_Hologram = abs(log10(ifftshift(X).^2));
 imagesc(Y_Hologram);
 title(['c = ', num2str(c)]);
+saveas(gcf, 'out/Step4Fig2.jpg');
+%% Lab 8 Addendum
+Hologram = imread('Lab8_Hologram.jpg');
+Reference = imread('Lab8_Reference.jpg');
+Object = imread('Lab8_Object.jpg');
+
+% Without subtraction
+Z = rgb2gray(Hologram);
+figure
+X = fft2(mat2gray(fftshift(Z)));
+Y_Hologram = abs(log10(ifftshift(X).^2));
+imagesc(Y_Hologram);
+saveas(gcf, 'out/Step5Fig1.jpg');
+
+% With subtraction
+Z = rgb2gray(Hologram)-(rgb2gray(Reference)+rgb2gray(Object));
+figure
+X = fft2(mat2gray(fftshift(Z)));
+Y_Hologram = abs(log10(ifftshift(X).^2));
+imagesc(Y_Hologram);
+saveas(gcf, 'out/Step5Fig2.jpg');
+
+x = linspace(-1,1,length(Hologram));
+y = linspace(-1,1,length(Hologram));
+[X_grid, Y_grid] = meshgrid(x,y);
+
+% % To find best looking phase
+% for c = -4:.2:4
+% grid_phase = exp(c * (X_grid.^2 + Y_grid.^2));
+% 
+% G_Hologram = double(rgb2gray(Hologram)) .* grid_phase;
+% Z = uint8(G_Hologram) - (rgb2gray(Reference)+rgb2gray(Object));
+% figure
+% 
+% X = fft2(mat2gray(fftshift(Z)));
+% Y_Hologram = abs(log10(ifftshift(X).^2));
+% imagesc(Y_Hologram);
+% title(['c = ', num2str(c)]);
+% end
+
+c = -2.8;
+grid_phase = exp(c * (X_grid.^2 + Y_grid.^2));
+
+G_Hologram = double(rgb2gray(Hologram)) .* grid_phase;
+Z = uint8(G_Hologram) - (rgb2gray(Reference)+rgb2gray(Object));
+figure
+
+X = fft2(mat2gray(fftshift(Z)));
+Y_Hologram = abs(log10(ifftshift(X).^2));
+imagesc(Y_Hologram);
+title(['c = ', num2str(c)]);
+saveas(gcf, 'out/Step5Fig2.jpg');
